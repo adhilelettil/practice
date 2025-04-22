7a3b85df422734829800a274bd5006a711d0edeb
muhammed adhil <madhil0770@gmail.com>
2025-04-22 10:49:26 +0530

    updatedm

--- my-app/src/pages/about_us/index.tsx
+++ my-app/src/pages/about_us/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './about.css';
 
 export default function index() {
   return (
-    <div>
+    <div className='about_us'>
       haloo its about page
     </div>
   )
