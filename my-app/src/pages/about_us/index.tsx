9dded538891fc48a40cb1c633d4988e7bb270075
muhammed adhil <madhil0770@gmail.com>
2025-04-22 11:08:05 +0530

    updatedm

--- my-app/src/pages/about_us/index.tsx
+++ my-app/src/pages/about_us/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './about.css';
+import React from 'react';
+import styles from './about.module.css';
 
 export default function index() {
   return (
-    <div className='about_us'>
+    <div className={styles.about_us}>
       haloo its about page
     </div>
-  )
+  );
 }
